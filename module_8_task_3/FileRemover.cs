b31ef591e4b7d1c6db9a11fe4e9147f0df0551cd
Eddy <volkikushaytnogi@gmail.com>
2023-12-15 22:20:58 +0100

    reworked unit 8.3 for my work purposes

--- module_8_task_3/FileRemover.cs
+++ module_8_task_3/FileRemover.cs
@@ -1,4 +1,6 @@
 ﻿using module_8_task_2;
+using System.IO.Compression;
+using System.Runtime.CompilerServices;
 
 namespace module_8_task_3
 {
@@ -31,6 +33,138 @@ namespace module_8_task_3
                 }
             }
             return sizeOfRemovedFiles;
+        }
+
+        public static bool FolderByNameChecker(DirectoryInfo diInfo, string name)
+        {
+            return true ? (diInfo.Name == name) : false;   
+        }
+        public static void FolderByNameRemover(DirectoryInfo directory, string name)
+        {
+            var directories = directory.GetDirectories();
+            foreach (DirectoryInfo d in directories)
+            {
+                var subderictories = d.GetDirectories();
+                foreach(DirectoryInfo sub in subderictories)
+                {
+                    if(FolderByNameChecker(sub, name))
+                    {
+                        sub.Delete(true);
+                    }
+                }
+            }
+        }
+
+        public static void FileMover(DirectoryInfo mainDirectory)
+        {
+            var directoriesInStudentFolder = mainDirectory.GetDirectories();
+            foreach( DirectoryInfo oneLevelDeeper in directoriesInStudentFolder)
+            {
+                foreach(DirectoryInfo TwoLevelDeeper in oneLevelDeeper.GetDirectories())
+                { 
+                    if(TwoLevelDeeper.Name == oneLevelDeeper.Name)
+                    {
+                        foreach(FileInfo file in TwoLevelDeeper.GetFiles())
+                        {
+                            string newName = $"{oneLevelDeeper.FullName}\\{file.Name}";
+                            file.MoveTo(newName);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void EmptyFolderRemover(DirectoryInfo directory)
+        {
+            if(directory.GetDirectories().Length == 0 && directory.GetFiles().Length == 0)
+            {
+                directory.Delete(true);
+            }
+            else
+            {
+                foreach(var subDirectory in directory.GetDirectories())
+                {
+                    EmptyFolderRemover (subDirectory);
+                }
+            }
+        }
+
+        public static void FilesByExtensionMover(DirectoryInfo directory, string extension, string subFolderName)
+        {
+            foreach(FileInfo file in directory.GetFiles())
+            {
+                if(file.Extension == extension)
+                {   string newName = $"{directory.FullName}\\{subFolderName}\\{file.Name}";
+                    file.MoveTo(newName);
+                }
+            }
+        }
+
+        public static void ArchivesPreparer(DirectoryInfo directory, string nameIsThisDamnMacosx)
+        { 
+            var zipfiles = directory.GetFiles();
+            var zipfilePath = zipfiles[0].FullName;
+            ZipFile.ExtractToDirectory(zipfilePath, directory.FullName);
+            foreach(var dir in directory.GetDirectories())
+            {
+                if(FolderByNameChecker(dir, nameIsThisDamnMacosx))
+                {
+                    dir.Delete(true);
+                }
+            }
+            string subfolderForOriginalArchivesName = "Оригинальные архивы";
+            if (!Directory.Exists($"{directory.FullName}\\{subfolderForOriginalArchivesName}"))
+            {
+                directory.CreateSubdirectory(subfolderForOriginalArchivesName);
+            }
+            string newMainZipFileName = $"{directory.FullName}\\{subfolderForOriginalArchivesName}\\{zipfiles[0].Name}";
+            zipfiles[0].MoveTo(newMainZipFileName);
+        }
+
+        public static void ArchivesExtracter(DirectoryInfo directory)
+        {
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                var tempString = file.Name.Remove(file.Name.Length - 4, 4);
+                var dirForStudent = $"{directory.FullName}\\{tempString}";
+                if (!Directory.Exists(dirForStudent))
+                {
+                    directory.CreateSubdirectory(tempString);
+                }
+            }
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                string pathToArchive = file.FullName;
+                var tempString = file.Name.Remove(file.Name.Length - 4, 4);
+                ZipFile.ExtractToDirectory (pathToArchive, $"{directory.FullName}\\{tempString}");
+            }
+        }
+
+        public static string FolderPreparer(DirectoryInfo directory) 
+        {
+            if (directory.GetFiles().Length > 1)
+            {
+                Exception onlyOneFileMustBeInFolder = new("НЕ БОЛЬШЕ ОДНОГО ФАЙЛА");
+                {
+                    throw onlyOneFileMustBeInFolder;
+                    
+                };
+                
+            }
+            else
+            {
+                var mainArchive = directory.GetFiles()[0];
+                string currentFlowName = $"{directory.FullName}\\{mainArchive.Name.Remove(mainArchive.Name.Length - 4, 4)}";
+                if (!Directory.Exists(currentFlowName))
+                {
+                    directory.CreateSubdirectory($"{mainArchive.Name.Remove(mainArchive.Name.Length - 4, 4)}");
+                    
+                }
+                var currentFlow = new DirectoryInfo(currentFlowName);
+                mainArchive.MoveTo($"{currentFlow.FullName}\\{mainArchive.Name}");
+                return currentFlowName;
+            }
+
         }
     }
 }
--- module_8_task_3/Program.cs
+++ module_8_task_3/Program.cs
@@ -8,9 +8,10 @@ namespace module_8_task_3
     {
         static void Main(string[] args)
         {
+
+            /* код для задания 8.3
             Console.WriteLine("Введите путь до папки, которую нужно очистить: ");
-            string path = Console.ReadLine();
-            bool isPathOkFilesExist = PathAndInsidesChecker.IsPathOkAndFilesExist(path);
+            string path = Console.ReadLine();  
             if (isPathOkFilesExist)
             {
                 var workFolder = new DirectoryInfo(path);
@@ -20,6 +21,34 @@ namespace module_8_task_3
                 Console.WriteLine($"Во время очистки было удалено файлов: {removedFilesCount}, общий размер удаленных файлов: {bytesRemoved}");
                 long sizeAfterCleaning = FilesSizeCounter.SizeCounter(workFolder, out filesCount);
                 Console.WriteLine($"Размер папки после очистки: {sizeAfterCleaning}, файлов: {filesCount}");
+            }*/
+
+            //нужный мне для работы код
+            string path = "C:\\Users\\Eddy\\Downloads\\wrk\\PWS\\ПРОВЕРЯЕМ";
+            string readyPath = "C:\\Users\\Eddy\\Downloads\\wrk\\PWS\\проверено";
+            bool isPathOkFilesExist = PathAndInsidesChecker.IsPathOkAndFilesExist(path);
+            if (isPathOkFilesExist )
+            {   
+                var mainCourseFolder = new DirectoryInfo(path);
+                try 
+                {
+                    string currentFlowWorkFolderPath = FileRemover.FolderPreparer(mainCourseFolder);
+                    var currrentFlowWorkFolder = new DirectoryInfo(currentFlowWorkFolderPath);
+                    FileRemover.ArchivesPreparer(currrentFlowWorkFolder, nameIsThisDamnMacosx: "__MACOSX");
+                    FileRemover.ArchivesExtracter(currrentFlowWorkFolder);
+                    FileRemover.FolderByNameRemover(currrentFlowWorkFolder, name: "__MACOSX");
+                    FileRemover.FileMover(currrentFlowWorkFolder);
+                    FileRemover.EmptyFolderRemover(currrentFlowWorkFolder);
+                    FileRemover.FilesByExtensionMover(currrentFlowWorkFolder, extension: ".zip", subFolderName: "Оригинальные архивы");
+                    Console.WriteLine("UNPACKING AND CLEANING DONE! ");
+                    Console.WriteLine("PRESS ANY KEY TO MARK FOLDER AS READY: ");
+                    Console.ReadKey();
+                    currrentFlowWorkFolder.MoveTo($"{readyPath}\\{currrentFlowWorkFolder.Name}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message); 
+                }
             }
         }
     }
